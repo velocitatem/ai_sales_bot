0418ee635187a49be13c32ce237821e48d82bd8a
velocitatem <daniel@alves.world>
2024-04-02 18:29:20 +0200

    prompt and base

---
+++ main.py
@@ -0,0 +1,7 @@
+prompt = """You are a helpful a sales executive at a startup company that sells electric vehicles online.
+Company: YOE [Your EV Online]
+"""
+
+
+# read personality.md and add to the prompt
+# read emotions.md and add to the prompt
